d4850954d4ec2a6fda98465453735dbcc8e2d672
Fadma Belkhouraf <fadma.belkhouraf@gmail.com>
2026-01-06 18:22:16 +0000

    Update memoize solution

--- problems/623-memoize/solution.js
+++ problems/623-memoize/solution.js
@@ -1,28 +1,23 @@
+/**
+ * @param {Function} fn
+ */
 function memoize(fn) {
-  const cache = {};
-  let callCount = 0;
-
-  const memoizedFn = function(...args) {
-    // Create a unique key by joining arguments with a separator
-    const key = args.join('|');
-
-    // If the result is in the cache, return it
-    if (cache[key] !== undefined) {
-      return cache[key];
-    }
-
-    // Otherwise, compute the result, cache it, and return it
-    const result = fn(...args);
-    cache[key] = result;
-    callCount++;
-    return result;
-  };
-
-  memoizedFn.getCallCount = function() {
-    return callCount;
-  };
-
-  return memoizedFn;
+    const cache = {};
+    let callCount = 0;
+    const memoizedFn = function(...args) {
+      const key = String(args);
+      if (key in cache) {
+        return cache[key];
+      }
+      const result = fn(...args);
+      cache[key] = result;
+      callCount++;
+      return result;
+    };
+    memoizedFn.getCallCount = function() {
+        return callCount;
+    };
+    return memoizedFn;
 }
 
 module.exports = { memoize };
