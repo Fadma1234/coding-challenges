116e1432bfe5f8726615d6a2a22f50d8d94d9bd1
Fadma Belkhouraf <fadma.belkhouraf@gmail.com>
2026-01-06 17:26:34 +0000

    Add solution for 2626. Array Reduce Transformation

--- problems/2626-array-reduce-transformation/solution.js
+++ problems/2626-array-reduce-transformation/solution.js
@@ -1,13 +1,15 @@
 /**
- * LeetCode 2626 — Array Reduce Transformation
- * User-provided implementation exported as `reduce`.
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let curr = init;
-    for (let i = 0; i < nums.length; i++) {
-        curr = fn(curr, nums[i]);
+    let res=init;
+    for(let i=0;i<nums.length;i++){
+        res=fn(res,nums[i])
     }
-    return curr;
+    return res
 };
 
 module.exports = { reduce };
