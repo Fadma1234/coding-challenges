434839e42ab2348c78b48f23cce3827be251180e
Fadma Belkhouraf <fadma.belkhouraf@gmail.com>
2026-01-06 17:48:47 +0000

    Add solution for 2666. Allow One Function Call

--- problems/2666-allow-one-function-call/solution.js
+++ problems/2666-allow-one-function-call/solution.js
@@ -5,14 +5,14 @@
 var once = function(fn) {
     let cnt=0;
     return function(...args){
-        if(cnt ===0)
-    { 
-    cnt++;
-    return fn(...args)
-}else{
-    return undefined;
-}
-}
+        if(cnt===0)
+        { 
+            cnt++
+            return fn(...args)
+    }else{
+        return undefined
+    }
+    }
 }
 
 module.exports = { once };
